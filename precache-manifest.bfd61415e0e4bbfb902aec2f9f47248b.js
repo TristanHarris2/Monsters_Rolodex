f750f21cd694204ea2fecd2040b299df711640df
Tristan Harris <htristan893@gmail.com>
2022-05-24 12:29:12 +0200

    Updates

---
+++ precache-manifest.bfd61415e0e4bbfb902aec2f9f47248b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a5a491cc764dc780370",
+    "url": "//Monsters_Rolodex/static/css/main.a7908b22.chunk.css"
+  },
+  {
+    "revision": "6a5a491cc764dc780370",
+    "url": "//Monsters_Rolodex/static/js/main.6a5a491c.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "//Monsters_Rolodex/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "aeca49248a22eaf99f93",
+    "url": "//Monsters_Rolodex/static/js/2.aeca4924.chunk.js"
+  },
+  {
+    "revision": "3c2c2413522f24644210",
+    "url": "//Monsters_Rolodex/static/js/runtime~main.3c2c2413.js"
+  },
+  {
+    "revision": "42205229b442370cc3e0eca93a697d78",
+    "url": "//Monsters_Rolodex/index.html"
+  }
+];
